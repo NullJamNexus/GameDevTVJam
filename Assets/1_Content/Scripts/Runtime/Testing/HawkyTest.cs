749fceacadbe5acca1ffd399d141a13626d8b859
DeadShotNinja <Ukr816@gmail.com>
2024-05-25 16:31:31 -0600

    test fix

--- Assets/1_Content/Scripts/Runtime/Testing/HawkyTest.cs
+++ Assets/1_Content/Scripts/Runtime/Testing/HawkyTest.cs
@@ -4,43 +4,11 @@ using NJN.Runtime.Input;
 using UnityEngine;
 using Zenject;
 using Sirenix.OdinInspector;
-using ModestTree;
-using UnityEditorInternal;
-using Mono.CSharp;
-using Unity.VisualScripting;
-using NJN.Runtime.Components;
-using System.Runtime.CompilerServices;
 
-namespace NJN.Runtime.Testing{
-
-
-public class HawkyTest : MonoBehaviour
+namespace NJN.Runtime.Testing
 {
-        [Inject]
-        private IPlayerFactory _playerFactory;
+    public class HawkyTest : MonoBehaviour
+    {
         
-        [Inject]
-        private IInputProvider _inputProvider;
-
-        PlayerController player;
-
-        [SerializeField] bool playerIsClimbing;
-    
-
-
-    [Button("Spawn Player")]
-    private void SpawnPlayer(){
-        player = _playerFactory.Create();
-        player.transform.position = Vector2.zero;
-         _inputProvider.EnablePlayerControls();
-
     }
-
-    private void Update(){
-        
-       
-    }
-
-    
-}
 }
