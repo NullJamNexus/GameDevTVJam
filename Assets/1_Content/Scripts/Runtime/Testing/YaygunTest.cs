26573684d704ab33ba9659fae2165d547691a378
Yaygun <yusufaygunnn@hotmail.com>
2024-05-26 13:26:04 +0300

    YaygunTest
    YaygunProtoScene And Yaygun Test Script Created

---
+++ Assets/1_Content/Scripts/Runtime/Testing/YaygunTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NJN.Runtime.Controllers.Enemy;
+using NJN.Runtime.Controllers.Player;
+using NJN.Runtime.Factories;
+using NJN.Runtime.Input;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Zenject;
+
+namespace NJN.Runtime.Testing
+{
+    public class YaygunTest : MonoBehaviour
+    {
+        
+    }
+}
