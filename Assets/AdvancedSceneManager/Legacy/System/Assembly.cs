e6c81e47c01985a9650e7108de41e96052dcb262
DeadShotNinja <Ukr816@gmail.com>
2024-06-01 08:51:21 -0600

    removed ASM

--- Assets/AdvancedSceneManager/Legacy/System/Assembly.cs
+++
@@ -1,7 +0,0 @@
-using System.Runtime.CompilerServices;
-using UnityEngine.Scripting;
-
-[assembly: InternalsVisibleTo("AdvancedSceneManager.Legacy.Editor")]
-[assembly: InternalsVisibleTo("AssetDashboard")]
-[assembly: InternalsVisibleTo("AssetDashboardExtensions")]
-[assembly: Preserve]
--- Assets/AdvancedSceneManager/System/Utility/Callbacks/ISceneCallbacks.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AdvancedSceneManager.Callbacks
-{
-
-    /// <summary>Base interface for <see cref="ISceneOpen"/>, <see cref="ISceneClose"/>, <see cref="ICollectionOpen"/>, <see cref="ICollectionClose"/>. Does nothing on its own, used by <see cref="CallbackUtility"/>.</summary>
-    public interface ISceneCallbacks
-    { }
-
-}
