8fcc81ef2fc16a5ee092881da8e4f88793fcbeee
Yaygun <yusufaygunnn@hotmail.com>
2024-06-02 23:58:47 +0300

    Truck Wheel movement

--- Assets/1_Content/Scripts/Runtime/Components/truck_wheel/TruckWheel.cs
+++ Assets/1_Content/Scripts/Runtime/Components/truck_wheel/TruckWheel.cs
@@ -1,3 +1,5 @@
+using NJN.Runtime.Managers.Level.Signals;
+using System.Collections;
 using UnityEngine;
 using Zenject;
 
@@ -13,24 +15,53 @@ namespace NJN.Runtime.Components
             _signalBus = signalBus;
         }
 
-        private float _rotateSpeed=360;
-
+        private float _speed;
+        readonly private float _speedMultiplyConstant = 50;
+        readonly private float _accelerationMultiplyConstant = 30;
+        readonly private float _deaccelerationMultiplyConstant = 3.07f;
+  
         private void Start()
         {
-            
-        }
-        private void Update()
-        {
-            transform.Rotate(0,0,_rotateSpeed * Time.deltaTime);
+            _signalBus.Subscribe<DestinationTransitionStartedSignal>(TransitionStarted);
+            _signalBus.Subscribe<DestinationTransitionFinishedSignal>(TransitionEnded);
         }
         private void OnDestroy()
         {
-            
+            _signalBus.TryUnsubscribe<DestinationTransitionStartedSignal>(TransitionStarted);
+            _signalBus.TryUnsubscribe<DestinationTransitionFinishedSignal>(TransitionEnded);
         }
 
-        private void SetSpeed()
+        private void TransitionStarted(DestinationTransitionStartedSignal signal)
         {
+            StartCoroutine(Acceleration(signal.AccelerationRate, signal.MaxSpeed));
+        }
 
+        private void TransitionEnded(DestinationTransitionFinishedSignal signal)
+        {
+            StopAllCoroutines();
+            StartCoroutine(Deceleration(signal.DecelerationRate));
+        }
+
+        private IEnumerator Acceleration(float accelerate, float maxSpeed)
+        {
+            while (true)
+            {
+                _speed += accelerate * _accelerationMultiplyConstant * Time.deltaTime;
+                _speed = Mathf.Min(maxSpeed, _speed);
+                transform.Rotate(0, 0, _speed * _speedMultiplyConstant * Time.deltaTime);
+                yield return null;
+            }
+        }
+
+        private IEnumerator Deceleration(float DecelerationRate)
+        {
+            while (_speed > 0)
+            {
+                transform.Rotate(0, 0, _speed * _speedMultiplyConstant * Time.deltaTime);
+                _speed -= DecelerationRate * _deaccelerationMultiplyConstant * Time.deltaTime;
+                yield return null;
+            }
+            _speed = 0;
         }
     }
 }
