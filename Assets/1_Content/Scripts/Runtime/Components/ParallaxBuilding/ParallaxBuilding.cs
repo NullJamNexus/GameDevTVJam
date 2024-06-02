cc8adbf571db53afc19fd827a73bb83365b1c6ca
Yaygun <yusufaygunnn@hotmail.com>
2024-06-02 03:26:49 +0300

    Parallax Building part 2
    Start Movement must be played when truck started travelling.
    StopSpawner must be fired before starting to slow down

--- Assets/1_Content/Scripts/Runtime/Components/ParallaxBuilding/ParallaxBuilding.cs
+++ Assets/1_Content/Scripts/Runtime/Components/ParallaxBuilding/ParallaxBuilding.cs
@@ -1,77 +1,85 @@
-using Mono.CSharp;
-using NUnit.Framework;
-using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
-using Zenject;
 
-public class ParallaxBuilding : MonoBehaviour
+namespace NJN.Runtime.Components
 {
-    [SerializeField] private GameObject _prefabBuilding;
-    List<GameObject> _lBuildingList = new List<GameObject>();
-    float _moveSpeed = 5;
-
-    private float _spawnX = -5;
-    private float _destroyX = +20;
-    private float _spawnY = 2;
-
-    [SerializeField] private float NewSpeed = 1;
-
-    private Transform _newDestination;
-    void Start()
+    public class ParallaxBuilding : MonoBehaviour
     {
-        //StartMovement();
-    }
+        [SerializeField] private GameObject _prefabTransitionBuilding; // there should not be any collision or trigger or close player 
+        List<GameObject> _lBuildingList = new List<GameObject>();
+        float _maxSpeed = 25;
+        float _moveSpeed;
+        float _acceleration = 2;
 
-    [Button(ButtonSizes.Large)]
-    public void SetSpeed()
-    {
-        _moveSpeed = NewSpeed;
-    }
-    private void StartMovement()
-    {
-        StartCoroutine(Movement());
-        StartCoroutine(Spawner());
-    }
-    public void StopSpawner()
-    {
-        StopAllCoroutines();
-    }
+        private float _spawnX = -5;
+        private float _destroyX = +20;
+        private float _spawnY = 2;
 
-    private IEnumerator Spawner()
-    {
-        while (true)
+        private bool _spawnBuilding;
+
+        void Start()
         {
-            yield return new WaitForSeconds(2);
-            GameObject building = Instantiate(_prefabBuilding);
-            building.transform.position = new Vector3(_spawnX, _spawnY, 0); 
-            _lBuildingList.Add((building));
+            //StartMovement();
+        }
+
+        public void StartMovement()
+        {
+            _spawnBuilding = true;
+            _moveSpeed = 0;
+            StartCoroutine(SpeedUp());
+            StartCoroutine(Movement());
+            StartCoroutine(Spawner());
+        }
+        public void StopSpawner()
+        {
+            _spawnBuilding = false;
+        }
+
+        private IEnumerator Spawner()
+        {
+            while (_spawnBuilding)
+            {
+                yield return new WaitForSeconds(5); //randomize 5 is good for testing
+                GameObject building = Instantiate(_prefabTransitionBuilding);
+                building.transform.position = new Vector3(_spawnX, _spawnY, 0);
+                _lBuildingList.Add((building));
+            }
+        }
+
+        private IEnumerator SpeedUp()
+        {
+            while (_moveSpeed < _maxSpeed)
+            {
+                _moveSpeed += _acceleration * Time.deltaTime;
+                yield return null;
+            }
+            _moveSpeed = _maxSpeed;
+        }
+        private IEnumerator Movement()
+        {
+            while (true)
+            {
+                yield return null;
+                List<GameObject> lDestroyBuilding = new List<GameObject>();
+                foreach (GameObject Building in _lBuildingList)
+                {
+                    Vector3 pos = Building.transform.position;
+                    pos.x += _moveSpeed * Time.deltaTime;
+                    if (pos.x > _destroyX)
+                        lDestroyBuilding.Add(Building);
+                    else
+                        Building.transform.position = pos;
+                }
+                foreach (GameObject destroyBuilding in lDestroyBuilding)
+                {
+                    _lBuildingList.Remove(destroyBuilding);
+                    Destroy(destroyBuilding);
+                    if (_lBuildingList.Count == 0 && !_spawnBuilding)
+                        StopAllCoroutines();
+                }
+
+            }
         }
     }
-    private IEnumerator Movement()
-    {
-        while (true)
-        {
-            yield return null;
-            List<GameObject> lDestroyBuilding = new List<GameObject>();
-            foreach (GameObject Building in _lBuildingList)
-            {
-                Vector3 pos = Building.transform.position;
-                pos.x += _moveSpeed * Time.deltaTime;
-                if (pos.x>_destroyX)
-                    lDestroyBuilding.Add(Building);
-                else
-                    Building.transform.position = pos;  
-            }
-            foreach (GameObject destroyBuilding in lDestroyBuilding)
-            {
-                _lBuildingList.Remove(destroyBuilding);
-                Destroy(destroyBuilding);
-            }
-            
-        }
-    }
- 
 }
